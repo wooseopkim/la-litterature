3b517fb3759aa28ec747cee0880230135b5b32eb
Wooseop Kim <wooseop.kim.dev@gmail.com>
2020-12-21 21:56:10 +0900

    Add title input

--- src/components/posts/PostEditor.svelte
+++ src/components/posts/PostEditor.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-  import { Button } from 'carbon-components-svelte';
+  import { Button, TextInput } from 'carbon-components-svelte';
   import EditorJS from '@editorjs/editorjs';
   import tools from './editorjs-tools';
+
+  const titleMaxLength = 64;
   
   let editor: EditorJS;
   let target: HTMLElement;
+  let title: string;
 
   $: if (target) {
     editor = new EditorJS({
@@ -19,14 +22,18 @@
       minHeight: 0
     });
   }
+  $: if (title?.length > titleMaxLength) {
+    title = title.substring(0, titleMaxLength);
+  }
 
   async function onSubmit() {
     const data = await editor.save();
-    console.log(data);
+    console.log(title, data);
   }
 </script>
 
 <div class="layout">
+  <TextInput light hideLabel bind:value={title} />
   <div class="editor" bind:this={target} />
   <Button kind="secondary" on:click={onSubmit}>투고</Button>
 </div>
@@ -34,6 +41,13 @@
 <style>
   .layout {
     background-color: white;
+  }
+
+  .layout :global(input) {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    height: 3rem;
+    text-align: center;
   }
 
   .editor :global(.codex-editor__redactor) {
