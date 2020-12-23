2ff19c39c12eaf3fb9f2ccd64b5f3f669855d044
Wooseop Kim <wooseop.kim.dev@gmail.com>
2020-12-24 05:33:45 +0900

    Set up ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+const eslintSveltePreprocess = require('eslint-svelte3-preprocess');
+const svelteConfig = require('./svelte.config');
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'svelte3',
+  ],
+  overrides: [
+    {
+      files: ['src/**/*.svelte'],
+      processor: 'svelte3/svelte3',
+    }
+  ],
+	settings: {
+    'svelte3/preprocess': eslintSveltePreprocess(svelteConfig.preprocess)
+	},
+};
