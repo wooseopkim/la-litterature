d7133499aaeeb18a29059d869566dcb57dd3ad16
Wooseop Kim <wooseop.kim.dev@gmail.com>
2021-01-06 02:28:12 +0900

    Simplify test post list placeholder

--- src/app/components/posts/list/SlottedPostList.test.svelte
+++ src/app/components/posts/list/SlottedPostList.test.svelte
@@ -8,9 +8,6 @@
   <div slot="editor">편집기</div>
   <div slot="item" let:item>{item.title}</div>
   <div slot="placeholder" let:size>
-    {#each Array(size) as _}
-      ...
-      <br />
-    {/each}
+    {#each Array(size) as _}...{/each}
   </div>
 </SlottedPostList>
